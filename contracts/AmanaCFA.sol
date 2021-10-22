33737e5a9cb99152d65080c0e4142f94aff18d2e
Lmehdi <ouabi54@gmail.com>
2021-10-22 20:07:41 +0200

    Contracts

--- contracts/AmanaCFA.sol
+++ contracts/AmanaCFA.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract AmanaCFA is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("AmanaCFA", "ACFA", 18) public {
+    constructor(uint256 initialSupply) ERC20Detailed("AmanaCFA", "ACFA", 5) public {
         _mint(msg.sender, initialSupply**18);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,12 @@ const AmanaMarketplace = artifacts.require("AmanaMarketplace");
 
 
 module.exports = async function (deployer, network, accounts) {
-  // deployment steps
-  await deployer.deploy(AmanaCFA, 100000000000);
-  await deployer.deploy(AmanaProperty);
-  await deployer.deploy(AmanaMarketplace);
+  await deployer.deploy(AmanaCFA, 100000000000); // We deploy the token
+  await deployer.deploy(AmanaProperty); // We deploy the NFT contract
+  await deployer.deploy(AmanaMarketplace, AmanaCFA.address, AmanaProperty.address); // We deploy Marketplace
+
+  console.log('---------------------------------');
+  console.log('AmanaCFA -> ', AmanaCFA.address);
+  console.log('AmanaProperty -> ', AmanaProperty.address);
+  console.log('AmanaMarketplace -> ', AmanaMarketplace.address);
 };
